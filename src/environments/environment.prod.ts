61cc858c96ec1118e04bcd29149d0330a8f7271d
Martin <martin.z.atanasov@gmail.com>
2020-02-08 23:36:46 +0200

    some fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBqhmoPXYDzUM8jPithZRSaByZg5jotNeI',
+    authDomain: 'frt-task.firebaseapp.com',
+    databaseURL: 'https://frt-task.firebaseio.com',
+    projectId: 'frt-task',
+    storageBucket: 'frt-task.appspot.com',
+    messagingSenderId: '809503559004',
+    appId: '1:809503559004:web:39ddeb06bef6276e0fc599'
+  }
 };
